45299324002f8da1cab55add0f2e3b90c4fd43b1
VinnyLinda20 <vinnylindawaty@gmail.com>
2021-10-12 14:22:05 +0700

    Sesi2

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/{name}/{nrp}', function($name,$nrp) {
+    echo "Hello". "Nama". $name. "nrp".$nrp;
+})->where('name', '[A-Za-z]+') ->where('nrp', '[0-9]+');
+
+Route::get('/{nrp}/{name}', function($nrp,$name) {
+    echo "Hello". "nrp". $nrp. "nama".$nama;
+})->where('nrp', '[0-9]+') ->where('name', '[A-Za-z]+');
+
+Route::get('/cekbilangan/{bilangan}', function($bilangan) {
+    if($bilangan %2 == 0)
+        return "Bilangan tersebut Genap";
+    }else{
+        return "Bilangan tersebut Ganjil";
+    }
+});
